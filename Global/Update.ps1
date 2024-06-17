eac61e44cd025bb60b2cfb17acadb41ee6132d32
Arthur <aelion@viennedoc.com>
2024-06-17 10:39:54 +0200

    preparation pour la version 7

--- Global/Update.ps1
+++ Global/Update.ps1
@@ -21,6 +21,9 @@ Out-File -Encoding ASCII -InputObject $e -FilePath 'C:\Program Files\Zabbix Agen
 
 Move-Item -force -Path 'C:\Program Files\Zabbix Agent 2\zabbix_agent2_new.conf' -Destination 'C:\Program Files\Zabbix Agent 2\zabbix_agent2.conf'
 Invoke-WebRequest 'https://cdn.zabbix.com/zabbix/binaries/stable/6.4/6.4.12/zabbix_agent2-6.4.12-windows-amd64-openssl.msi' -OutFile 'C:\Program Files\Zabbix Agent 2\zabbix_agent2-X.X.X-windows-amd64-openssl.msi'
+
+#Invoke-WebRequest 'https://cdn.zabbix.com/zabbix/binaries/stable/7.0/7.0.0/zabbix_agent2-7.0.0-windows-amd64-openssl.msi' -OutFile 'C:\Program Files\Zabbix Agent 2\zabbix_agent2-X.X.X-windows-amd64-openssl.msi'
+
 msiexec.exe /i 'C:\Program Files\Zabbix Agent 2\zabbix_agent2-X.X.X-windows-amd64-openssl.msi' /log 'C:\Program Files\Zabbix Agent 2\zabbix_agent_install.log' /quiet
 
 Start-Sleep 20
